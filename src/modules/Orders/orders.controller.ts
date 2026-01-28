796ef787b3e3d93260e711171ef75f8352bc2580
fahimmuntacher <muntacherfahim234@gmail.com>
2026-01-28 21:23:17 +0600

    get all orders

--- src/modules/Orders/orders.controller.ts
+++ src/modules/Orders/orders.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { orderService } from "./order.service";
+import { paginationsSortingHelper } from "../../helpers/paginationsSortingHelper";
 
 const createOrder = async (req: Request, res: Response) => {
   try {
@@ -18,6 +19,35 @@ const createOrder = async (req: Request, res: Response) => {
   }
 };
 
+const getAllOrders = async (req: Request, res: Response) => {
+  try {
+    const search =
+      typeof req.query.search === "string" ? req.query.search : undefined;
+    const { page, limit, skip, sortBy, sortOrder } = paginationsSortingHelper(
+      req.query,
+    );
+
+    const result = await orderService.getAllOrders({
+      page,
+      limit,
+      skip,
+      sortBy,
+      sortOrder,
+      search,
+    });
+    res.status(200).json({
+      success: true,
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      error: "Orders retrieve failed",
+      details: error.message,
+    });
+  }
+};
+
 export const orderController = {
   createOrder,
+  getAllOrders,
 };
